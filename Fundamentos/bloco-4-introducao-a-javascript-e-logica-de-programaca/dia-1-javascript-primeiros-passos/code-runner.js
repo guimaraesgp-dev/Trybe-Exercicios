db4b2b04abd5974a6524a515283f4a54418fa46c
Gabriel Guimarães <gabrielguimaraes.dev@gmail.com>
2022-04-06 17:16:11 -0300

    Introdução em JS

---
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programaca/dia-1-javascript-primeiros-passos/code-runner.js
@@ -0,0 +1,8 @@
+let turma = 22;
+let company = 'Trybe';
+
+turma = 'Truma 22- Tribo B';
+company = 123;
+
+console.log(turma);
+console.log(company);
---
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programaca/dia-1-javascript-primeiros-passos/scripts.js
@@ -0,0 +1,1 @@
+console.log("Turma 22 - Tribo B");
