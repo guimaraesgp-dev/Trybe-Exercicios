ac43468b8c583cb9b8999bf535b5dc2229063f4b
Gabriel Guimarães <gabrielguimaraes.dev@gmail.com>
2022-04-06 17:42:44 -0300

    Exemplos condicionais

--- Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programaca/dia-1-javascript-primeiros-passos/code-runner.js
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programaca/dia-1-javascript-primeiros-passos/code-runner.js
@@ -1,8 +1,12 @@
 let turma = 22;
 let company = 'Trybe';
+let myBox;
+let box = null;
 
-turma = 'Truma 22- Tribo B';
+turma = 'Turma 22- Tribo B';
 company = 123;
 
 console.log(turma);
 console.log(company);
+console.log(myBox);
+console.log(box);
---
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programaca/dia-1-javascript-primeiros-passos/conditionals.js
@@ -0,0 +1,18 @@
+let number1 = 23;
+let number2 = 55;
+
+if (number1 > number2) {
+    console.log('The first number is bigger');
+} else {
+    console.log('The second number is bigger');
+}
+
+let age = 15;
+
+if (age >18) {
+    console.log('Can drive');
+} else if (age < 18) {
+    console.log('Can not driver');
+} else {
+    console.log('Can driver, but with restriction')
+}
