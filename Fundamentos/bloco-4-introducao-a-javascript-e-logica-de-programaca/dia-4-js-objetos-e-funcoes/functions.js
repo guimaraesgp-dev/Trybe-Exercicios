877711d9a54bfb9c0c5e711e889892b3118456d8
Gabriel Guimarães <gabrielguimaraes.dev@gmail.com>
2022-04-11 17:40:25 -0300

    Novos Exemplos

--- Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programaca/dia-4-js-objetos-e-funcoes/functions.js
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programaca/dia-4-js-objetos-e-funcoes/functions.js
@@ -16,3 +16,33 @@ function sum(numero1, numero2){
 }
 
 sum(1,2);
+
+//--------------------
+function max(a, b){
+    if (a>b){
+        return a;
+    }else {
+        return b;
+    }
+}
+
+console.log(mas(35,25));
+
+//-------------------
+function average(num1, num2, num3) {
+    return (num1 + num2 + num3) / 3;
+}
+
+console.log(avarege(10, 20, 30));
+
+//-----------------------
+function generateArray (){
+    let array = []
+
+    for (let index = 1; index <=100; index +=1){
+        array.push(index)
+    }
+    return array;
+}
+
+console.log(generateArray());
