88c1d4917458594af0a014b825b152b37c5afc19
Gabriel Guimarães <gabrielguimaraes.dev@gmail.com>
2022-05-02 10:35:47 -0300

    ATT

---
+++ Fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-2-bibliotecas-js-e-framework-css/script.js
@@ -0,0 +1,7 @@
+let button = document.getElementById('myModal');
+
+var myModal = new bootstrap.Modal(document.getElementById('exampleModal');
+
+button.addEventListener('click', function()) {
+    myModal.show()
+}
